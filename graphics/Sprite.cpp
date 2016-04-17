585ed4b1b5c2c55f3eeab70e9514ce0a4c1f91ce
Kirill Rodriguez <theoden8@gmail.com>
2016-04-17 15:47:13 +0100

    textures -> _textures

--- graphics/Sprite.cpp
+++ graphics/Sprite.cpp
@@ -6,6 +6,6 @@ Sprite::Sprite (std::string path, int framesCount):
 {
 	for(int i = 0; i < framesCount; i++) {
 		std::cerr << "\033[1;92mtextures/" + path + "_" + std::to_string(i) + ".tga\033[0m" << std::endl;
-		frames[i].load("textures/" + path + "_" + std::to_string(i) + ".tga");
+		frames[i].load("_textures/" + path + "_" + std::to_string(i) + ".tga");
 	}
 }
